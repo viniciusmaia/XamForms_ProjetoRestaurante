f358a998b58ab9e1cee898378d4245a4832f760e
LS\viniciusmaia <vinicius.c.maia@gmail.com>
2018-03-15 11:26:35 -0300

    Criação de binding para floating button

---
+++ VFoodProject/VFood/VFood.Android/CustomViews/FloatingActionButtonViewRenderer.cs
@@ -0,0 +1,153 @@
+﻿using Android.Content;
+using Android.Content.Res;
+using Android.Support.Design.Widget;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.IO;
+using VFood.CustomViews;
+using VFood.Droid.CustomViews;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(FloatingActionButtonView), typeof(FloatingActionButtonViewRenderer))]
+namespace VFood.Droid.CustomViews
+{
+    public class FloatingActionButtonViewRenderer : ViewRenderer<FloatingActionButtonView, FrameLayout>
+    {
+        private const int MARGIN_DIPS = 16;
+        private const int FAB_HEIGHT_NORMAL = 56;
+        private const int FAB_HEIGHT_MINI = 40;
+        private const int FAB_FRAME_HEIGHT_WITH_PADDING = (MARGIN_DIPS * 2) + FAB_HEIGHT_NORMAL;
+        private const int FAB_FRAME_WIDTH_WITH_PADDING = (MARGIN_DIPS * 2) + FAB_HEIGHT_NORMAL;
+        private const int FAB_MINI_FRAME_HEIGHT_WITH_PADDING = (MARGIN_DIPS * 2) + FAB_HEIGHT_MINI;
+        private const int FAB_MINI_FRAME_WIDTH_WITH_PADDING = (MARGIN_DIPS * 2) + FAB_HEIGHT_MINI;
+        private readonly FloatingActionButton _floatingButton;
+
+        public Command Command { get; set; }
+
+        public FloatingActionButtonViewRenderer(Context Context) : base(Context)
+        {
+            float d = Context.Resources.DisplayMetrics.Density;
+            var margin = (int)(MARGIN_DIPS * d); // margin in pixels
+
+            _floatingButton = new FloatingActionButton(Context);
+            var lp = new FrameLayout.LayoutParams(LayoutParams.WrapContent, LayoutParams.WrapContent);
+            lp.Gravity = GravityFlags.CenterVertical | GravityFlags.CenterHorizontal;
+            lp.LeftMargin = margin;
+            lp.TopMargin = margin;
+            lp.BottomMargin = margin;
+            lp.RightMargin = margin;
+            _floatingButton.LayoutParameters = lp;
+            Clickable = true;
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<FloatingActionButtonView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null || this.Element == null)
+                return;
+
+            if (e.OldElement != null)
+                e.OldElement.PropertyChanged -= HandlePropertyChanged;
+
+            if (this.Element != null)
+            {
+                //UpdateContent ();
+                this.Element.PropertyChanged += HandlePropertyChanged;
+            }
+
+            Element.Show = Show;
+            Element.Hide = Hide;
+
+            SetFabImage(Element.ImageName);
+            SetFabSize(Element.Size);
+
+            
+            
+            _floatingButton.Click += (sender, args) =>
+            {
+                if (Element.Command == null)
+                {
+                    Element.FloatingClicked();
+                }
+                else
+                {
+                    Element.Command.Execute(null);
+                }
+
+            };
+
+            var frame = new FrameLayout(Context);
+            frame.RemoveAllViews();
+            frame.AddView(_floatingButton);
+
+            SetNativeControl(frame);
+        }
+
+        public void Show()
+        {
+            _floatingButton.Show();
+        }
+
+        public void Hide()
+        {
+            _floatingButton.Hide();
+        }
+
+        void HandlePropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "Content")
+            {
+                Tracker.UpdateLayout();
+            }
+            else if (e.PropertyName == FloatingActionButtonView.ImageNameProperty.PropertyName)
+            {
+                SetFabImage(Element.ImageName);
+            }
+            else if (e.PropertyName == FloatingActionButtonView.BackgroundTintProperty.PropertyName)
+            {
+                _floatingButton.BackgroundTintList = ColorStateList.ValueOf( Element.BackgroundTint.ToAndroid());
+            }
+            else if (e.PropertyName == FloatingActionButtonView.SizeProperty.PropertyName)
+            {
+                SetFabSize(Element.Size);
+            }
+        }
+
+        void SetFabImage(string imageName)
+        {
+            if (!string.IsNullOrWhiteSpace(imageName))
+            {
+                try
+                {
+                    var drawableNameWithoutExtension = Path.GetFileNameWithoutExtension(imageName);
+                    var resources = Context.Resources;
+                    var imageResourceName = resources.GetIdentifier(drawableNameWithoutExtension, "drawable", Context.PackageName);
+                    _floatingButton.SetImageBitmap(Android.Graphics.BitmapFactory.DecodeResource(Context.Resources, imageResourceName));
+                }
+                catch (Exception ex)
+                {
+                    throw new FileNotFoundException("There was no Android Drawable by that name.", ex);
+                }
+            }
+        }
+
+        void SetFabSize(FloatingActionButtonSize size)
+        {
+            if (size == FloatingActionButtonSize.Mini)
+            {
+                _floatingButton.Size = (int)FloatingActionButtonSize.Mini;
+                Element.WidthRequest = FAB_MINI_FRAME_WIDTH_WITH_PADDING;
+                Element.HeightRequest = FAB_MINI_FRAME_HEIGHT_WITH_PADDING;
+            }
+            else
+            {
+                _floatingButton.Size = (int)FloatingActionButtonSize.Normal;
+                Element.WidthRequest = FAB_FRAME_WIDTH_WITH_PADDING;
+                Element.HeightRequest = FAB_FRAME_HEIGHT_WITH_PADDING;
+            }
+        }
+    }
+}
